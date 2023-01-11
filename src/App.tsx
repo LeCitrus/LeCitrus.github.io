0bfe51d4878c777f302729eb53539dc5e5cf36cf
LeCitrus <curtis.tl.kan@gmail.com>
2023-01-11 12:30:16 -0700

    adding navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 
+// https://stackoverflow.com/questions/41956465/how-to-create-multiple-page-app-using-react
 function App() {
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div className="App">
+        <h1>Curtis Kan</h1>
+      </div>
+      <div className="Navbar">
+        {/* https://react-bootstrap.github.io/components/navbar/ */}
+        <Navbar bg="light" expand="lg">
+          <Container>
+            <Navbar.Brand href="#home">LeCitrus</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="#aboutme">About me</Nav.Link>
+              <Nav.Link href="#projects">Projects</Nav.Link>
+              <Nav.Link href="#hobbies">Hobbies</Nav.Link>
+            </Nav>
+          </Container>
+        </Navbar>
+      </div>
     </div>
   );
 }
